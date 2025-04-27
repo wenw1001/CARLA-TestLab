0989462d0aa8f7a779232108b866271e510e816d
wenw1001 <winnie973314@gmail.com>
2025-04-27 20:46:24 +0800

    chore: make small adjustments

--- PythonAPI/examples/generate_traffic.py
+++ PythonAPI/examples/generate_traffic.py
@@ -206,6 +206,7 @@ def main():
 
         spawn_points = world.get_map().get_spawn_points()
         number_of_spawn_points = len(spawn_points)
+        print(f"(wen) There are {number_of_spawn_points} spawn points:")
 
         if args.number_of_vehicles < number_of_spawn_points:
             random.shuffle(spawn_points)
